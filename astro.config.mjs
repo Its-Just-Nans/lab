bd29f670fb6d27d363bfa584c91b23ae50bc4d17
n4n5 <its.just.n4n5@gmail.com>
2024-12-13 23:29:26 +0100

    bump

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import rehypeKatex from "rehype-katex";
-import rehypeExternalLinks from "rehype-external-links";
-import remarkMath from "remark-math";
-import remarkToc from "remark-toc";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
@@ -13,15 +8,6 @@ export default defineConfig({
     site: "https://n4n5.dev/lab/",
     integrations: [
         svelte(),
-        mdx({
-            remarkPlugins: [remarkMath, remarkToc],
-            rehypePlugins: [rehypeKatex, [rehypeExternalLinks, { target: "_blank" }]],
-            shikiConfig: {
-                theme: "github-dark",
-                // // Enable word wrap to prevent horizontal scrolling
-                // wrap: true,
-            },
-        }),
         sitemap({
             changefreq: "weekly",
             priority: 0.7,
